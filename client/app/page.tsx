abc870bb694fc0f120402cbbd5b7b1b87ce2797c
Brendan Chan <brendan.mk.chan@gmail.com>
2024-07-10 03:18:43 -0700

    Updated landing page.
    Updated landing page with appropriate text to introduce the user to the
    website.

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,12 +3,8 @@ import Bubble from '../components/Bubble';
 import BubbleContainer from '../components/BubbleContainer';
 
 const texts: string[] = [
-    "Hello world, testing",
-    "This is a test",
-    "Abracadabra",
-    "Nah, I'd win",
-    "Nah, I'd gamble",
-    "Jorgambler > Lefraud",
+    "Welcome! This is a web application designed to allow you to access a set of machine learning models to place a prediction on the outcome of sports games.",
+    "To begin, pick a sport (NBA only at the moment) and choose two teams. Once done, press the simulate button to receive an outcome!",
 ];
 
 export default function Home() {
