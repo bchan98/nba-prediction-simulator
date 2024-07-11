998aa5e9da28c4e47e965b9a9b304e328ce70e6c
Brendan Chan <brendan.mk.chan@gmail.com>
2024-07-11 05:06:49 -0700

    Added dockerfile/docker-compose.yml
    Added dockerfile to allow site to be deployed as a container. Still need
    to fix errors.

--- client/components/SportsMatchScreen.tsx
+++ client/components/SportsMatchScreen.tsx
@@ -24,19 +24,21 @@ export default function SportsMatchScreen() {
     };
 
     function determineWinner() {
-        axios.get(`http://localhost:5000/api/get-winner?team1Name=${team1.name}&team2Name=${team2.name}`)
-            .then(response => {
-                console.log('Match winner:', response.data);
-                if (response.data.winner === String(team1.name)) {
-                    setWinner(team1);
-                }
-                else {
-                    setWinner(team2);
-                }
-            })
-            .catch(error => {
-                console.error('Error getting match winner:', error);
-            });
+        if (team1 && team2) {
+            axios.get(`http://127.0.0.1:5000/api/get-winner?team1Name=${team1.name}&team2Name=${team2.name}`)
+                .then(response => {
+                    console.log('Match winner:', response.data);
+                    if (response.data.winner === String(team1.name)) {
+                        setWinner(team1);
+                    }
+                    else {
+                        setWinner(team2);
+                    }
+                })
+                .catch(error => {
+                    console.error('Error getting match winner:', error);
+                });
+        }
     }
 
     return (
