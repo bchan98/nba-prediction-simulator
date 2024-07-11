1af7d753bf33706d995b74bcac529eb12d98cd47
Brendan Chan <brendan.mk.chan@gmail.com>
2024-07-10 19:57:38 -0700

    Updated website styling.
    Removed template background image - replaced with generic pattern from
    heropatterns. Updated colors of homebar/text color to match rest of
    website.

--- client/components/Homebar.tsx
+++ client/components/Homebar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Homebar() {
     return (
-        <div className="fixed top-10 left-10 w-32 h-auto m-0 flex flex-col bg-emerald-700 text-white shadow-lg rounded-md">
+        <div className="fixed top-10 left-10 w-32 h-auto m-0 flex flex-col bg-gray-200 text-black shadow-lg rounded-md">
             <ul className="flex flex-col justify-center items-center">
                 <li className="mb-2 md:p-4"><img src="/img/logo.png" /></li>
                 <li className="mb-2 md:p-4 text-lg"><a href="/">Home</a></li>
