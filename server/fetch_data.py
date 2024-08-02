b1aa60bc303a89b1f68be2f1b29537d32878397f
Brendan Chan <brendan.mk.chan@gmail.com>
2024-08-01 21:25:27 -0700

    Added API Fetch function.
    Previously, API was fetched per each prediction call. As games only
    change once per day, this will be called once at the end of each day to
    ensure minimum calls required.

---
+++ server/fetch_data.py
@@ -0,0 +1,21 @@
+from nba_api.stats.endpoints import leaguedashteamstats
+import pandas as pd
+import os
+from dotenv import load_dotenv
+
+# load_dotenv()
+
+# http_proxy = os.getenv("HTTP_PROXY")
+
+
+def fetch_data():
+    request = leaguedashteamstats.LeagueDashTeamStats(
+        last_n_games="5", per_mode_detailed="PerGame"
+    )
+    fetchedStatistics = request.get_data_frames()[0]
+    fetchedStatistics.to_csv("nba_stats.csv")
+
+
+if __name__ == "__main__":
+    print("Script is being run.")
+    fetch_data()
